320e55542f3db4180167e340a8d7a9c3d7a48a50
bsillorequez <bryansillorequez@gmail.com>
2020-06-22 10:53:10 -0700

    Setup user auth with frontend structure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def require_login
+    render json: ['Not Logged In'], status: 404 unless logged_in?
+  end
+
+  def login(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout
+    current_user.reset_session_token! if logged_in?
+    session[:session_token] = nil
+    @current_user = nil
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
